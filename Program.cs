4e579e76639eedf1b023c8caa11a89201aed2fbd
Justin Hainline <Justin.Hainline@mssa.newhorizons.com>
2024-06-18 11:08:39 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualBasic;
+
+namespace Assignment_7._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the number of elements: ");
+            int n = int.Parse(Console.ReadLine());
+
+            int[] array = new int[n];
+            Console.WriteLine("Enter the values: ");
+
+            for (int i = 0; i < n; i++)
+            {
+                array[i] = int.Parse(Console.ReadLine());
+            }
+
+            ShellSort(array, n);
+            Console.WriteLine("Sorted array:");
+             
+            foreach (var element in array)
+            {
+                Console.Write(element + " ");
+            }
+        }
+
+        static void ShellSort(int[] array, int size)
+        {
+            for (int gap = size / 2; gap > 0; gap /= 2)
+            {
+                for (int i = gap; i < size; i++)
+                {
+                    int temp = array[i];
+                    int j = i;
+
+                    while (j >= gap && array[j - gap] > temp)
+                    {
+                        array[j] = array[j - gap];
+                        j -= gap;
+                    }
+                    array[j] = temp;
+                }
+            }
+        }
+    }
+}
